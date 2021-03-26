3544dcbc31ceaaefa5f384da73d387338ff97990
jb1685 <joshua.caleb.bender@gmail.com>
2021-03-26 04:19:20 -0400

    update chatroom design with bootstrap cards

--- chatroom/models.py
+++ chatroom/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 class Chat(models.Model):
-    sender_uuid = models.CharField(max_length=50)
-    receiver_uuid = models.CharField(max_length=50)
+    to_uuid = models.CharField(max_length=50)
+    from_uuid = models.CharField(max_length=50)
     message = models.CharField(max_length=1000)
     seen = models.BooleanField()
