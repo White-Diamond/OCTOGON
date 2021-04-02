eeecb1700e4967b0e2864aaa554356a69cf36978
ryanhummel <ryan.j.hummel43@gmail.com>
2021-04-02 16:38:00 -0400

    Initial message/post/thread database with basic testing.

--- posts/models.py
+++ posts/models.py
@@ -4,17 +4,17 @@ import datetime
 
 class Thread(models.Model):
     thread_ID = models.IntegerField(default=0)
-    thread_date = models.DateTimeField('Posted: ')
+    thread_date = models.DateTimeField(auto_now_add=True)
     threadTopic = models.CharField(max_length=200, default=' ')
     currentPostNumber = models.IntegerField(default=0)
-    main_post_id = models.IntegerField(default=0)  #might be a better way to do this later
+    main_post_id = models.IntegerField(default=0)
 
 
 class Post(models.Model):
     thread = models.ForeignKey(Thread, on_delete=models.CASCADE) #this should by default add post_set to Thread
     post_ID = models.IntegerField(default=0)
     user_ID = models.IntegerField(default=0)
-    post_date = models.DateTimeField('published:')
+    post_date = models.DateTimeField(auto_now_add=True)
     main_text = models.TextField()
 
 
@@ -22,5 +22,5 @@ class Message(models.Model):
     message_ID = models.IntegerField(default=0)
     sender_ID = models.IntegerField(default=0)
     recipient_ID = models.IntegerField(default=0)
-    message_date = models.DateTimeField('published:')
+    message_date = models.DateTimeField(auto_now_add=True)
     main_text = models.TextField()
