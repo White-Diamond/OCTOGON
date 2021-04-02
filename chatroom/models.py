2e1ee46e758c4bad762d69f1a54585f1479b878e
jb1685 <joshua.caleb.bender@gmail.com>
2021-04-02 16:24:30 -0400

    updated message database and front end polling

--- chatroom/models.py
+++ chatroom/models.py
@@ -3,11 +3,11 @@ from django.db import models
 # Create your models here.
 class Chat(models.Model):
     to_id = models.CharField(max_length=50)
-    from_id = models.CharField(max_length=50)
+    from_id = models.CharField(max_length=50) 
 
-class Message():
+class Message(models.Model):
     chat = models.ForeignKey(Chat, on_delete=models.CASCADE)
-    message = models.CharField(max_length=1000)
-    seen = models.BooleanField()
-    created_at = models.DateTimeField()
-    count = models.IntegerField()
+    message = models.CharField(max_length=5000)
+    seen = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+
--- chatroom/views.py
+++ chatroom/views.py
@@ -1,15 +1,50 @@
 from django.shortcuts import render
 from django.http import JsonResponse
 from django.http import HttpResponse
+from .models import Chat, Message
+import json
 
 # Create your views here.
 def chat_page(request):
     return render(request, "index.html")
 
 def retrieve_json(request):
-    return JsonResponse({'foo':'retrieve_json'})
+    # save messages in request.body
+    data = json.loads(request.body)
+    recipient_id = data['recipient']['username']
+    sender_id = data['sender']['username']
+
+    # find chatroom ID in Chat model
+    chatID = Chat.objects.get(to_id=sender_id, from_id=recipient_id)
+
+    # get all messages with chat id
+    chatMessages = Message.objects.filter(chat=chatID)
+
+    # get an ordered QuerySet of all unseen messages
+    orderedMessagesByDate = chatMessages.filter(seen=0).order_by('created_at').values('message')
+
+    # convert to QuerySet to JSON string
+    jsonMessages = json.dumps(list(orderedMessagesByDate))
+
+    # since messages have no been see update seen to 
+    orderedMessagesByDate.update(seen=1)
+
+    return HttpResponse(jsonMessages)
 
 def load_json(request):
     # save messages in request.body
-    print(request.body)
+    data = json.loads(request.body)
+    message = data['message']
+    recipient_id = data['recipient']['username']
+    sender_id = data['sender']['username']
+
+    # find chatroom ID in Chat model
+    chat = Chat.objects.get(to_id=recipient_id, from_id=sender_id)
+
+    # add message to Message model
+    newMessage = Message()
+    newMessage.message = message
+    newMessage.chat = chat
+    newMessage.save()
+
     return HttpResponse("OK")
