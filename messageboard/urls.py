15d9f489fc9564b85af4ab17598bcd62d7f4d36f
ryanhummel <ryan.j.hummel43@gmail.com>
2021-04-02 19:56:01 -0400

    Added Comments to models.py & tests.py and added a README

--- messageboard/urls.py
+++ messageboard/urls.py
@@ -1,14 +1,14 @@
 from django.urls import path
 
-from messageboard.views import basicResponse
-from messageboard.views import mainBoard
-from messageboard.views import getThreadPosts
-from messageboard.views import userMakesThread
-from post.models import Thread
-
+# from messageboard.views import basicResponse
+# from messageboard.views import mainBoard
+# from messageboard.views import getThreadPosts
+# from messageboard.views import userMakesThread
+# from posts.models import Thread
+#
 urlpatterns = [
-    path('basicRes/', basicResponse),
-    path('messageboard/', mainBoard),
-    path('thread/<int:thrdID>/', getThreadPosts),
-    path('threadcreate/', userMakesThread),
-]
+#     path('basicRes/', basicResponse),
+#     path('messageboard/', mainBoard),
+#     path('thread/<int:thrdID>/', getThreadPosts),
+#     path('threadcreate/', userMakesThread),
+ ]
--- octogon/settings.py
+++ octogon/settings.py
@@ -38,8 +38,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'posts',
-    'dajaxice',
-    'dajax',
     'messageboard',
 ]
 
