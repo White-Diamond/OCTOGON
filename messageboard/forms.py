41ee299fe3cfaa8b30072f04a4ceaaed1d9c9038
jb1685 <joshua.caleb.bender@gmail.com>
2021-04-03 00:19:43 -0400

    reset

--- messageboard/forms.py
+++
@@ -1,8 +0,0 @@
-from django import forms
-
-# form for a user to start a new thread on the 
-# messageboard
-class createThreadForm (forms.Form):
-    threadTopic = forms.CharField()
-    post_text = forms.TextInput()
-    
