1fa08cfd64b0a18179d135ded7002d1a33ba007d
Squirrelofudun <JMail12534@gmail.com>
2021-04-23 17:07:55 -0400

    minor update
    minor update

--- user_profile_db/views.py
+++ user_profile_db/views.py
@@ -1,3 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+
+from .models import Course, User
 
 # Create your views here.
+def class_selection(request, 
