df0932fe249f663423b7cc5e75a0a7f4094cd02b
Rooklyn Kline <rookkline99@gmail.com>
2021-04-29 15:23:08 -0400

    moved profile info

--- accounts/views.py
+++ accounts/views.py
@@ -24,14 +24,14 @@ def signup(request):
     if form.is_valid():
       user = form.save()
 
-      #profile = profile_form.save(commit=False)
-      #profile.user = user
-      #profile.save()
-      
       username = form.cleaned_data.get('username')
 
       group = Group.objects.get(name='student')
       user.groups.add(group)
+      
+      #profile = profile_form.save(commit=False)
+      #profile.user = user
+      #profile.save()
 
       messages.success(request, 'Account was created for ' + username)
       return redirect('login')
