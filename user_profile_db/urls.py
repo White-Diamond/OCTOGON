b8d39a18303be41d3b9bfd9c5840e86e5f71beb0
Squirrelofudun <JMail12534@gmail.com>
2021-05-05 17:37:43 -0400

    class->course
    renamed instances of class to course

--- user_profile_db/urls.py
+++ user_profile_db/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 app_name = 'user_profile_db'
 urlpatterns = [
-    path('addclass/', views.add_class),
-    path('removeclass/', views.remove_class),
-    path('manageclasses/', views.class_selection),
+    path('addcourse/', views.add_course),
+    path('removecourse/', views.remove_course),
+    path('managecourses/', views.course_selection),
 ]
--- user_profile_db/views.py
+++ user_profile_db/views.py
@@ -4,13 +4,13 @@ from .models import Course, Profile
 from .forms import add_course_form, remove_course_form
 
 # Create your views here.
-def class_selection(request):
+def course_selection(request):
     current_user = request.user
     # if current_user.is_authenticated
     context = {}
-    return render(request, "class_selection.html", context)
+    return render(request, "course_selection.html", context)
 
-def add_class(request):
+def add_course(request):
     current_user = request.user
     if request.method == 'POST':
         context = {}
@@ -21,13 +21,13 @@ def add_class(request):
             course = Course.objects.get(name_short=course_name)
             request.user.profile.courses.add(course)
             request.user.save()
-            return render(request, "add_class.html", context)
+            return render(request, "add_course.html", context)
     else:
         form = add_course_form(user=request.user)
     context = {}
     context['form'] = form
-    return render(request, "add_class.html", context)
-def remove_class(request):
+    return render(request, "add_course.html", context)
+def remove_course(request):
     current_user = request.user
     if request.method == 'POST':
         context = {}
@@ -38,9 +38,9 @@ def remove_class(request):
             course = Course.objects.get(name_short=course_name)
             request.user.profile.courses.remove(course)
             request.user.save()
-            return render(request, "remove_class.html", context)
+            return render(request, "remove_course.html", context)
     else:
         form = remove_course_form(user=request.user)
     context = {}
     context['form'] = form
-    return render(request, "remove_class.html", context)
+    return render(request, "remove_course.html", context)
