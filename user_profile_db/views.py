cabc4e27f1c8deec849234e1353ad0c6b9ac93f1
Squirrelofudun <JMail12534@gmail.com>
2021-05-06 21:53:06 -0400

    bar unauthenticated users from changing courses

--- user_profile_db/views.py
+++ user_profile_db/views.py
@@ -2,14 +2,17 @@ from django.shortcuts import render, get_object_or_404, redirect
 
 from profilepage.models import Course, Profile
 from .forms import add_course_form, remove_course_form
+from accounts.decorators import unauthenticated_user
 
 # Create your views here.
+@unauthenticated_user
 def course_selection(request):
     current_user = request.user
     # if current_user.is_authenticated
     context = {}
     return render(request, "course_selection.html", context)
 
+@unauthenticated_user
 def add_course(request):
     current_user = request.user
     if request.method == 'POST':
@@ -27,6 +30,8 @@ def add_course(request):
     context = {}
     context['form'] = form
     return render(request, "add_course.html", context)
+
+@unauthenticated_user
 def remove_course(request):
     current_user = request.user
     if request.method == 'POST':
